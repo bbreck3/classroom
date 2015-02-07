c74bcead4c02f36bd23d94bfba9920b26bdde433
=bbreck3 <=bbreck3@gmail.com>
2015-02-07 18:09:57 -0500

    Screen 1

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,13 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+/**
+ * 		The below was supposed to open the "class.xml" for testing purposes. This js file is the one that specifies things to be shown even before the "index.xml" is shown.
+ * 
+ * 				 it does not work, the coompler gives: "Error while parsing XML file"
+ * 
+ * 				It works fine without the "class.xml" but when adding the "class.xml" to the application neither "index.xml" nor "class.xmk" works
+ * 
+ */
+//$.class.open();
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,13 @@
 function doClick(e) {
     alert($.label.text);
 }
-
+/**
+ * 		The below was supposed to open the "class.xml" for testing purposes. 
+ * 
+ * 				 it does not work, the coompler gives: "Error while parsing XML file"
+ * 
+ * 				It works fine without the "class.xml" but when adding the "class.xml" to the application neither "index.xml" nor "class.xmk" works
+ * 
+ */
+//$.class.open();
 $.index.open();
